c8164d40fc48668ff9e860e7b52b384935ce065f
Incheol Seong 성인철 <sxin2949@neofect.com>
2022-12-23 17:03:39 +0900

    feat: localization 추가

--- new_chemion_color/lib/main.dart
+++ new_chemion_color/lib/main.dart
@@ -1,5 +1,12 @@
+// ignore_for_file: unused_field
+
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:get/get.dart';
 
@@ -15,15 +22,51 @@ Future<void> mainInit() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
   await Get.putAsync(() => AppConfigService().init());
+  await EasyLocalization.ensureInitialized();
 
+  // 화면 회전 막음.
   await SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
-  runApp(GetMaterialApp(
-    initialBinding: PreviewBinding(),
-    initialRoute: Routes.initial,
-    getPages: AppPages.pages,
-    themeMode: ThemeMode.light,
-    debugShowCheckedModeBanner: false,
-  ));
+  runZonedGuarded(() {
+    runApp(EasyLocalization(
+        supportedLocales: const [Locale('en', 'US'), Locale('ko', 'KR')],
+        path: 'assets/translations',
+        fallbackLocale: const Locale('en', 'US'),
+        child: Phoenix(child: const ChemionApp())));
+  }, (error, stack) => debugPrint(error.toString()));
+  // (error, stack) => FirebaseCrashlytics.instance.recordError(
+  //       error,
+  //       stack,
+  //       fatal: true,
+  //     ));
+}
+
+/// 케미온 메인 앱
+class ChemionApp extends StatefulWidget {
+  const ChemionApp({Key? key}) : super(key: key);
+
+  @override
+  State<ChemionApp> createState() => _ChemionAppState();
+}
+
+class _ChemionAppState extends State<ChemionApp> {
+  /// 인터넷 연결 확인
+  final ConnectivityResult _connectionStatus = ConnectivityResult.none;
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      initialBinding: PreviewBinding(),
+      initialRoute: Routes.initial,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      getPages: AppPages.pages,
+      themeMode: ThemeMode.light,
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
